7998428ba74293782ae9f44fdfe2d7c55058d846
Isaac Verbrugge <135486733+personontheinternet1234@users.noreply.github.com>
2025-09-13 16:41:27 -0400

    idk at this pt

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from ServoController import ServoController
 from CameraController import CameraController
 from flask import Flask, Response
+import time
 import cv2
 
 app = Flask(__name__)
@@ -16,14 +17,14 @@ def generate_frames():
 
             yield (b'--frame\r\n'
                    b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
-            
+
 @app.route('/')
 def index():
     return "<h1>Live Video Stream</h1><img src='/video_feed'>"
 
 @app.route('/video_feed')
 def video_feed():
-    return Response(generate_frames(),  
+    return Response(generate_frames(),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == "__main__":
@@ -31,6 +32,7 @@ if __name__ == "__main__":
     servoController.start_threads()
 
     cameraController = CameraController(servoController,"lol","lol")
+    time.sleep(1)
     cameraController.start_threads()
 
-    app.run(host='172.20.10.7', port=5000, debug=True)
+    app.run(host='172.20.10.7', port=5000, debug=False)
