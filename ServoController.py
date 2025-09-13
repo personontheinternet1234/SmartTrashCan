183700d002f768a6219ae077614f4ef3904832eb
Sophie <112041730+sophiebunn@users.noreply.github.com>
2025-09-13 12:29:16 -0400

    servo code

--- ServoController.py
+++ ServoController.py
@@ -5,5 +5,42 @@ class ServoController:
 
     def __init__(self):
         kit = ServoKit(channels=16, frequency=333)
-        kit.servo[0].angle = 0
-        kit.servo[1].angle = 0
+
+        self.angle1 = 0
+        self.angle2 = 0
+        self.status = None
+
+        self.servo1 = kit.servo[0]
+        self.servo2 = kit.servo[1]
+
+        self.servo1.angle = self.angle1
+        self.servo2.angle = self.angle2
+
+    def sefAngle1(self, angle):
+        if angle < 0 or angle > 180:
+            return
+        
+        self.angle1 = angle
+        self.servo1.angle = self.angle1
+    
+    def setAngle2(self, angle):
+        if angle < 0 or angle > 180:
+            return
+        
+        self.angle2 = angle
+        self.servo2.angle = self.angle2
+
+    def updatePlate(self):
+        #add actual plate values here
+        if self.status == "recycle":
+            self.setAngle1(0)
+            self.setAngle2(90)
+
+        elif self.status == "trash":
+            self.setAngle1(90)
+            self.setAngle2(0)
+        
+    def run(self):
+        ...
+
+        
