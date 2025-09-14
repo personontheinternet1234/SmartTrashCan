dc919e90e9281fe2c914c98ed54756ee41076319
Sophie <112041730+sophiebunn@users.noreply.github.com>
2025-09-14 10:24:27 -0400

    angle

--- ServoController.py
+++ ServoController.py
@@ -42,7 +42,7 @@ class ServoController:
 
     def updatePlate(self):
         if self.status == "trash":
-            self.setAngle(self.neutralAngle + self.angleOffset, 0)
+            self.setAngle(self.neutralAngle - self.angleOffset, 0)
             self.setAngle(self.neutralAngle + self.angleOffset, 1)
             self.setAngle(self.neutralAngle - self.angleOffset, 2)
             self.disposals += 1
@@ -50,12 +50,12 @@ class ServoController:
                 f.write(str(self.disposals))
         elif self.status == "recycle":
             self.setAngle(self.neutralAngle - self.angleOffset, 0)
-            self.setAngle(self.neutralAngle + self.angleOffset, 1)
+            self.setAngle(self.neutralAngle - self.angleOffset, 1)
             self.setAngle(self.neutralAngle + self.angleOffset, 2)
         elif self.status == "food":
             self.setAngle(self.neutralAngle + self.angleOffset, 0)
             self.setAngle(self.neutralAngle - self.angleOffset, 1)
-            self.setAngle(self.neutralAngle + self.angleOffset, 2)
+            self.setAngle(self.neutralAngle - self.angleOffset, 2)
         elif self.status == "none":
             # no need to do anything
             self.setAngle(self.neutralAngle, 0)
